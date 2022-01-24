3792da47db12a1e060b3e4bd4129c45d27edb089
Joseph <josinho.seven@gmail.com>
2022-01-24 06:48:26 +0530

    Added initial files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,45 @@
+const logo = require("../assets/img/logo.png");
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        <img src={logo} alt="Logo" className="header__logo" />
+        <span className="header__title-main">JS Quiz</span>
+      </h1>
+      <div className="header__title-sub">
+        Welcome to JavaScript quiz. Let's prove your skill level !!!
+      </div>
+      <div className="header__user-info">
+        <div>
+          <input
+            type="text"
+            className="header__user-info-textbox"
+            placeholder="Enter your name"
+          />
+        </div>
+        <ul className="radio-group">
+          <li className="radio-group__items">
+            <input type="radio" name="level" id="radio-beginner" />
+            <label htmlFor="radio-beginner">Beginner</label>
+          </li>
+          <li className="radio-group__items">
+            <input type="radio" name="level" id="radio-intermediate" />
+            <label htmlFor="radio-intermediate">Intermediate</label>
+          </li>
+          <li className="radio-group__items">
+            <input type="radio" name="level" id="radio-master" />
+            <label htmlFor="radio-master">Master</label>
+          </li>
+        </ul>
+        <div className="button-holder">
+          <a href="#" className="button large">
+            Let's begin
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
